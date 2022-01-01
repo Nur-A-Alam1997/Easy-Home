db3d2c268ae3c8d545e029ce2f59cfba76b7850d
nur <nuraalamp@gmail.com>
2022-01-01 23:22:45 +0600

    update and description file Updated

--- EasyHome.py
+++ EasyHome.py
@@ -1,16 +1,6 @@
 
 from EasyHome import app
 
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = "Easyhome_db"
-
-# mysql = MySQL(app)
-
-# APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-# jinja_env = Environment(extensions=['jinja2.ext.loopcontrols'])
-
 
 if __name__ == '__main__':
     app.secret_key='secret123'
--- EasyHome/route.py
+++ EasyHome/route.py
@@ -2,35 +2,40 @@ import gc
 import os
 
 from EasyHome import app
-from flask import render_template, request, flash, session, redirect, url_for, send_from_directory
-# from flask_mysqldb import MySQL
+from flask import render_template, request, flash, session, redirect, url_for, send_from_directory, jsonify
 from wtforms import form
-# from passlib.hash import sha256_crypt
 from functools import wraps
-# import sqlite3
+import sqlite3
 from EasyHome.register import RegisterForm
 from EasyHome import db
 
 
-@app.route('/',methods=['GET'])
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('logintransparent'))
+    return wrap
+
+
+@app.route('/', methods=['GET'])
 def home():
-    # with sqlite3.connect('database.db') as conn:
-    #     cursor = conn.cursor()
-    data=db.engine.execute(
+    data = db.engine.execute(
         "SELECT address,housetype,rentfee,id from post_ad_table").fetchall()
     db.session.commit()
     print(data)
-    # db.session.commit()
-    # # 
     x = len(data)
-    if x>6:
+    if x > 6:
         l = x - 6
     else:
-        l=0
+        l = 0
     print(l)
-    if x>6:
+    if x > 6:
         li = range(x-6, x)
-        li= [*li]
+        li = [*li]
         li.reverse()
     else:
         li = range(0, x)
@@ -44,36 +49,29 @@ def home():
         b = str(data[d][3]) + ".jpg"
         img.append(b)
 
-    # for c in img:
-    #     print(c)
-
     img = [*img]
     img.reverse()
     print(img)
-    return render_template("index.html",data=data,li=li, img=img, l=l)
+    return render_template("index.html", data=data, li=li, img=img, l=l)
 
-@app.route('/profile',methods=['GET'])
+
+@app.route('/profile', methods=['GET'])
 def profile():
-# cursor = mysql.connection.cursor()
-# with sqlite3.connect('database.db') as conn:
-#     cursor = conn.cursor()
-    data=db.engine.execute(
-        "SELECT name,username,email,Mobile_no,Address from registration_table  WHERE username = ?",[session['username']]).fetchall()
+    data = db.engine.execute(
+        "SELECT name,username,email,Mobile_no,Address from registration_table  WHERE username = ?", [session['username']]).fetchall()
     db.session.commit()
-    
-    #profile newsfeed
-    data_n =db.engine.execute(
+    data_n = db.engine.execute(
         "SELECT address,housetype,rentfee,id from post_ad_table WHERE username=?", [session['username']]).fetchall()
     db.session.commit()
-    
+
     x = len(data_n)
     print(x)
 
     img = []
 
-    if x>6:
+    if x > 6:
         li = range(x-6, x)
-        li= [*li]
+        li = [*li]
         li.reverse()
     else:
         li = range(0, x)
@@ -85,15 +83,15 @@ def profile():
         print(data_n[d][3])
         img.append(str(data_n[d][3])+".jpg")
 
-    if x>6:
+    if x > 6:
         l = x - 6
     else:
-        l=0
+        l = 0
     img = [*img]
     img.reverse()
 
     usr_image = session['username']+'.jpg'
-    return render_template("profile.html",data=data,data_n=data_n,li=li, img = img, l=l, usr_image = usr_image)
+    return render_template("profile.html", data=data, data_n=data_n, li=li, img=img, l=l, usr_image=usr_image)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -104,164 +102,116 @@ def edit_profile():
         name = request.form['name']
         email = request.form['email']
         mobileno = request.form['mobileno']
-        address= request.form['address']
+        address = request.form['address']
 
-        print(name)
-        print(email)
-        print(mobileno)
-        print(address)
+        db.engine.execute("UPDATE registration_table SET name=?, email =?, Mobile_no = ?, Address=? WHERE username= ?",
+                          (name, email, mobileno, address, [session['username']]))
 
-        # Create cursor
-        # cur = mysql.connection.cursor()
-        
-        db.engine.execute("UPDATE registration_table SET name=?, email =?, Mobile_no = ?, Address=? WHERE username= ?"
-                    , (name, email, mobileno,address,[session['username']]))
-
-        # Commit to DB
         db.session.commit()
 
         return redirect(url_for('profile'))
     return render_template('edit_profile.html')
 
 
-
-@app.route('/description/<string:id>', methods = ['GET'])
+@app.route('/description/<string:id>', methods=['GET'])
 def description(id):
     homeId = id
-    # homeId = 12
     id_n = homeId.split()
-    print(id)
-    # cursor = mysql.connection.cursor()
-    g_data=db.engine.execute(
+    g_data = db.engine.execute(
         "SELECT username,address,housetype,contactinformation,id from post_ad_table WHERE id=?", id).fetchone()
 
-    fab_id=db.engine.execute(
-         "SELECT id from fav_tab WHERE id=? AND username=?", (int(id),session['username'])).fetchone()
-    print("FAB---",fab_id)
-    des_data=db.engine.execute(
-        "SELECT description from post_ad_table WHERE id=?", id).fetchall()
-     
-    img = id+'.jpg'
-    return render_template("description.html",g_data=g_data,des_data=des_data,filename=img,fab_id=fab_id)
-
-
-@app.route('/update', methods = ['GET'])
-def update():
-    homeId = request.args.get('query')
-    print("HOME",homeId)
-    id_n = homeId.split()
-    print("IDn:",id_n[2])
-    # homeId = 12
-    # id_n = homeId.split()
-    # print(id_n)
-    # cursor = mysql.connection.cursor()
-    # conn = sqlite3.connect('database.db')
-    # cursor = conn.cursor()
-    
-    print("hole",int(id_n[2]),session['username'])
-
-    fab_id=db.engine.execute(
-         "SELECT id from fav_tab WHERE id=? AND username=?", (int(id_n[2]),session['username'])).fetchone()
-    
-    print("favourite",fab_id,"USername",session['username'], sep="---")
-    if fab_id:
-        db.engine.execute("DELETE FROM fav_tab WHERE id=? AND username=?",(int(id_n[2]),session['username']))
-
-    # cursor1 = mysql.connection.cursor()
+    if session.get("username"):
+        fab_id = db.engine.execute(
+            "SELECT id from fav_tab WHERE id=? AND username=?", (int(id), session['username'])).fetchone()
     else:
-        # g_data = db.engine.execute(
-        #  "SELECT * from post_ad_table WHERE id=? AND username=?", (int(id_n[2]),session['username'])).fetchone()
-        # print(g_data)
+        fab_id = None
+    des_data = db.engine.execute(
+        "SELECT description from post_ad_table WHERE id=?", id).fetchall()
+
+    img = id+'.jpg'
+    return render_template("description.html", g_data=g_data, des_data=des_data, filename=img, fab_id=fab_id)
+
+
+@app.route('/update', methods=['GET'])
+def update():
+    if not session.get('logged_in'):
+        data = {"error": "please,log in"}
+        res = jsonify(data)
+        # responseText
+        return "please,log in", 403
+    homeId = request.args.get('query')
+    id_n = homeId.split()
+    fab_id = db.engine.execute(
+        "SELECT id from fav_tab WHERE id=? AND username=?", (int(id_n[2]), session['username'])).fetchone()
+
+    print("favourite", fab_id, "USername", session['username'], sep="---")
+    if fab_id:
+        db.engine.execute("DELETE FROM fav_tab WHERE id=? AND username=?", (int(
+            id_n[2]), session['username']))
+    else:
         db.engine.execute(
             "INSERT INTO fav_tab(id ,username) VALUES(?, ?)",
-            ((int(id_n[2]),session['username'])))
-        # db.engine.execute(
-        #     "INSERT INTO fav_tab(id,address, housetype, description, rentfee, contactinformation, division, district, area, username) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        #     (g_data[0], g_data[1], g_data[2], g_data[3], g_data[4], g_data[5], g_data[6], g_data[7], g_data[8],g_data[9]))
-
+            ((int(id_n[2]), session['username'])))
     db.session.commit()
 
-    # Close connection
-    cursor.close()
-    #
-    # db.engine.execute(
-    #     "SELECT description from post_ad_table WHERE id=?", [id_n[2]])
-    # des_data = cursor.fetchall()
-    # img = id_n[2]+'jpg'
-    return "1"
+    return 'success',201
 
 
 
-
-@app.route('/favourite',methods=['GET', 'POST'])
+@app.route('/favourite', methods=['GET', 'POST'])
 def favourite():
+    if not session.get("username"):
+        return 'Sorry Sir, log in first'
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    db.engine.execute(
+        "SELECT address,housetype,rentfee,id FROM fav_tab")
+    data = cursor.fetchall()
 
-        # Create cursor
-        # cur1 = mysql.connection.cursor()
+    x = len(data)
+    print(x)
 
-        # Get user by username
-        conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
-        db.engine.execute(
-            "SELECT address,housetype,rentfee,id FROM fav_tab")
-        data=cursor.fetchall()
+    img = []
+    if x > 6:
+        li = range(x - 6, x)
+        li = [*li]
+        li.reverse()
+    else:
+        li = range(0, x)
+        li = [*li]
+        li.reverse()
 
-        x= len(data)
-        print(x)
+    for d in li:
+        print(d)
+        print(data[d][3])
+        img.append(str(data[d][3]) + ".jpg")
 
-        img = []
-        if x > 6:
-            li = range(x - 6, x)
-            li = [*li]
-            li.reverse()
-        else:
-            li = range(0, x)
-            li = [*li]
-            li.reverse()
+    if x > 6:
+        l = x - 6
+    else:
+        l = 0
+    img = [*img]
+    img.reverse()
 
-        for d in li:
-            print(d)
-            print(data[d][3])
-            img.append(str(data[d][3]) + ".jpg")
-
-        if x > 6:
-            l = x - 6
-        else:
-            l = 0
-        img = [*img]
-        img.reverse()
-
-        return render_template("favourite.html",data=data,li=li,img = img, l=l)
+    return render_template("favourite.html", data=data, li=li, img=img, l=l)
 
 
 
-#
-# @app.route('/des_test', methods = ['GET'])
-# def des_test():
-#     return render_template("des_test.html")
-
-@app.route('/search/<string:division>',methods=['GET', 'POST'])
+@app.route('/search/<string:division>', methods=['GET', 'POST'])
 def search_dhaka(division):
-    # if request.method == 'GET':
-    #     return render_template("search_"+division+".html")
-    if request.method == 'POST':
-        # Get Form Fields
-        division=division.upper()
+    if request.method == 'POST' and session.get("username"):
+        division = division.upper()
         area = request.form.get('area')
         housetype = request.form.get('housetype')
 
-        # Create cursor
-        # cur1 = mysql.connection.cursor()
-
-        # Get user by username
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
         db.engine.execute(
             "SELECT address,housetype,rentfee,id FROM post_ad_table where (housetype,division, area)=( ?, ?, ?)",
             (housetype, division, area))
-        data=cursor.fetchall()
+        data = cursor.fetchall()
 
-        x= len(data)
+        x = len(data)
         print(x)
 
         img = []
@@ -286,19 +236,14 @@ def search_dhaka(division):
         img = [*img]
         img.reverse()
 
-        return render_template("search_dhaka.html",data=data,li=li,img = img, l=l)
+        return render_template("search_dhaka.html", data=data, li=li, img=img, l=l)
     return render_template("search_"+division+".html")
-
-
-
-
 
 
 
 @app.route('/post_ad', methods=['GET', 'POST'])
 def post_ad():
-    if request.method == 'POST':
-        # Get Form Fields
+    if request.method == 'POST' and session.get("username"):
         address = request.form['address']
         housetype = request.form.get('housetype')
         description = request.form['description']
@@ -309,25 +254,18 @@ def post_ad():
         area = request.form.get('area')
         username = [session['username']]
 
-        print(type(address),housetype,description,rentfee,contactinformation,division,district,area,username[0])
+        print(type(address), housetype, description, rentfee,
+              contactinformation, division, district, area, username[0])
 
-        # Create cursor
-        # cur = mysql.connection.cursor()
-
-        #conn=sqlite3.connect("database.db")
-        #cursor=conn.cursor()
-    # Execute query
         db.engine.execute(
             "INSERT INTO post_ad_table( address, housetype, description, rentfee, contactinformation, division, district, area, username) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
             (address, housetype, description, rentfee, contactinformation, division, district, area, username[0]))
-        
+
         db.engine.execute(
             "SELECT id FROM post_ad_table where ( address, housetype, description, rentfee, contactinformation, division, district, area, username)=(?, ?, ?, ?, ?, ?, ?, ?, ?)",
             (address, housetype, description, rentfee, contactinformation, division, district, area, username[0]))
         data = cursor.fetchall()
-        print("MY DATA",data)
-
-        # print(data[0][0])
+        print("MY DATA", data)
         Id = str(data[0][0])+".jpg"
         print(Id)
 
@@ -355,7 +293,6 @@ def post_ad():
         db.session.commit()
 
         # Close connection
-        
 
         return redirect("http://127.0.0.1:5000/")
     return render_template("post_ad.html")
@@ -365,18 +302,22 @@ def post_ad():
 def send_image(filename):
     return send_from_directory("images", filename)
 
+
 @app.route('/upload/<filename>')
 def send_image1(filename):
     return send_from_directory("images", filename)
+
 
 @app.route('/<filename>')
 def send_image2(filename):
     return send_from_directory("images", filename)
 
 # User loginmain
+
+
 @app.route('/loginmain', methods=['GET', 'POST'])
 def loginmain():
-    if is_logged_in:
+    if session.get("username"):
         return redirect(url_for("home"))
     if request.method == 'POST':
         # Get Form Fields
@@ -386,8 +327,9 @@ def loginmain():
         # Create cursor
         # cur = mysql.connection.cursor()
         with conn:
-        # Get user by username
-            result = db.engine.execute("SELECT * FROM registration_table WHERE username = ?", [username])
+            # Get user by username
+            result = db.engine.execute(
+                "SELECT * FROM registration_table WHERE username = ?", [username])
 
             if result > 0:
                 # Get stored hash
@@ -414,17 +356,19 @@ def loginmain():
     return render_template('loginmain.html')
 
 # Check if user logged in
-def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Unauthorized, Please login', 'danger')
-            return redirect(url_for('logintransparent'))
-    return wrap
+# def is_logged_in(f):
+#     @wraps(f)
+#     def wrap(*args, **kwargs):
+#         if 'logged_in' in session:
+#             return f(*args, **kwargs)
+#         else:
+#             flash('Unauthorized, Please login', 'danger')
+#             return redirect(url_for('logintransparent'))
+#     return wrap
 
 # Logout
+
+
 @app.route('/logout')
 @is_logged_in
 def logout():
@@ -437,7 +381,7 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-   return render_template("dashboard.html")
+    return render_template("dashboard.html")
 
 
 @app.route('/registertrans', methods=['GET', 'POST'])
@@ -452,19 +396,19 @@ def registertrans():
         # password = sha256_crypt.encrypt(str(form.password.data))
         password = str(form.password.data)
         mobileno = form.mobileno.data
-        address= form.address.data
-
+        address = form.address.data
 
         # Create cursor
         # cur = mysql.connection.cursor()
-        #conn=sqlite3.connect("database.db")
-        #cursor=conn.cursor()
+        # conn=sqlite3.connect("database.db")
+        # cursor=conn.cursor()
         # Execute query
-        db.engine.execute("INSERT INTO registration_table( name, username, email, password, Mobile_no, address) VALUES(?, ?, ?, ?, ?, ?)", (name,  username, email, password, mobileno, address))
+        db.engine.execute("INSERT INTO registration_table( name, username, email, password, Mobile_no, address) VALUES(?, ?, ?, ?, ?, ?)",
+                          (name,  username, email, password, mobileno, address))
 
         db.engine.execute(
-            "SELECT username FROM registration_table where (username)=(?)",[username]).fetchall()
-        
+            "SELECT username FROM registration_table where (username)=(?)", [username]).fetchall()
+
         print(data[0][0])
         Id = str(data[0][0]) + ".jpg"
         print(Id)
@@ -480,21 +424,12 @@ def registertrans():
             print(upload)
             print("{} is the file name".format(upload.filename))
             filename = upload.filename
-
-            # Id = request.form['Id']
-            # Id = Id + ".jpg"
             destination = "/".join([target, Id])
             print("Accept incoming file:", filename)
             print("Save it to:", destination)
             upload.save(destination)
-        # Commit to DB
         db.session.commit()
-
-        # Close connection
-        
-
         flash('You are now registered and can log in', 'success')
-
         return redirect("http://127.0.0.1:5000/")
     return render_template('registertrans.html', form=form)
 
@@ -503,36 +438,25 @@ def registertrans():
 @app.route('/logintransparent', methods=['GET', 'POST'])
 def logintransparent():
     if request.method == 'POST':
-        # Get Form Fields
         username = request.form['username']
         password_candidate = request.form['password']
-
-        # Create cursor
-        # cur = mysql.connection.cursor()
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
-        # Get user by username
-        result = db.engine.execute("SELECT * FROM registration_table WHERE username = ?", [username]).fetchone()
-        print("my result:",result)
-        if result :
-            # Get stored hash
-            data = cursor.fetchone()
-            password = data[3]
 
-            # Compare Passwords
-            # if sha256_crypt.verify(password_candidate, password):
+        result = db.engine.execute(
+            "SELECT * FROM registration_table WHERE username = ?", [username]).fetchone()
+        print("my result:", result)
+        if result:
+            data = cursor.fetchone()
+            password = result[3]
             if password_candidate == password:
-                # Passed
                 session['logged_in'] = True
                 session['username'] = username
-
                 flash('You are now logged in', 'success')
                 return redirect("http://127.0.0.1:5000/")
             else:
                 error = 'Invalid login'
                 return render_template('logintransparent.html', error=error)
-            # Close connection
-            
         else:
             error = 'Username not found'
             return render_template('logintransparent.html', error=error)
